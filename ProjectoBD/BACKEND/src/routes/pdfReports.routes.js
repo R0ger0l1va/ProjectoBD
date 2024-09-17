11645fd4d2d7ef0384a6563845a5903d1d7b9d45
Roger Oliva <145022854+R0ger0l1va@users.noreply.github.com>
2024-09-17 09:40:07 -0400

    asd
    asdasd

--- ProjectoBD/BACKEND/src/routes/pdfReports.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { pdf_getAgencia } from "../controllers/reportsPDF.controllers.js";
-
-const router = Router();
-
-router.get("/gen-pdf_getAgencia/:id", pdf_getAgencia);
-
-export default router;
--- ProjectoBD/README.md
+++
@@ -1,1 +0,0 @@
-OK Proyecto de Base de Datos con tema de polizas de seguro (Pal Carajo) SE necesita organizacion, todo en carpetas separadas y bien nombradas
